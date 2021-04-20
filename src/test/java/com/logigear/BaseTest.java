d2f5cc7be6cf85e9eb62979d5c929fb2b6f66824
thienan090196 <thienan090196@gmail.com>
2021-04-20 17:35:09 +0700

    Add BaseTest class

---
+++ src/test/java/com/logigear/BaseTest.java
@@ -0,0 +1,26 @@
+package com.logigear;
+
+import com.logigear.helper.Constant;
+import com.logigear.helper.web_driver_helper.DriverManagerFactory;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    @BeforeTest
+    public void beforeMethod() {
+        Constant.DRIVER_MANAGER = DriverManagerFactory.getDriverManager(DriverManagerFactory.DriverType.CHROME);
+        Constant.WEB_DRIVER = Constant.DRIVER_MANAGER.getWebDriver();
+        Constant.WEB_DRIVER.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        Constant.WEB_DRIVER.get(Constant.RAILWAY_URL);
+    }
+
+    @AfterTest
+    public void afterMethod() {
+        Constant.WEB_DRIVER.manage().deleteAllCookies();
+        Constant.DRIVER_MANAGER.quitDriver();
+    }
+
+}
