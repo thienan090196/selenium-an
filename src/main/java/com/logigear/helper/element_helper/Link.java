f347ca02d0203eb10ecbd0323142cc40faae7ef3
thienan090196 <thienan090196@gmail.com>
2021-04-26 16:05:43 +0700

    add web element Link class

---
+++ src/main/java/com/logigear/helper/element_helper/Link.java
@@ -0,0 +1,10 @@
+package com.logigear.helper.element_helper;
+
+import org.openqa.selenium.By;
+
+public class Link extends BaseElement {
+
+    public Link(By locator) {
+        super(locator);
+    }
+}
