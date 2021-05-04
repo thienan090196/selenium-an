ab23a01466f0b979cd06f7a1cde408e6d84b6359
thienan090196 <thienan090196@gmail.com>
2021-05-04 14:16:10 +0700

    Add TC01 - User can log into Railway with valid username and password

---
+++ src/main/java/com/logigear/helpers/DriverHelper.java
@@ -0,0 +1,67 @@
+package com.logigear.helpers;
+
+import com.logigear.enums.DriverType;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverHelper {
+
+    private static final DriverHelper instance = new DriverHelper();
+
+    ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    private DriverHelper() {
+
+    }
+
+    public static DriverHelper getInstance() {
+        return instance;
+    }
+
+    public static void navigateToUrl(String url) {
+        getInstance().getDriver().get(url);
+    }
+
+    public WebDriver getDriver() {
+        return driver.get();
+    }
+
+    public void createDriver(DriverType driverType) {
+        WebDriver webDriver;
+        switch (driverType) {
+            case CHROME:
+                WebDriverManager.chromedriver().setup();
+                webDriver = new ChromeDriver();
+                break;
+            case FIREFOX:
+                WebDriverManager.firefoxdriver().setup();
+                webDriver = new FirefoxDriver();
+                break;
+            default:
+                WebDriverManager.chromedriver().setup();
+                webDriver = new EdgeDriver();
+                break;
+        }
+        webDriver.manage().timeouts().implicitlyWait(Constant.IMPLICITLY_WAIT_TIME, TimeUnit.SECONDS);
+        driver.set(webDriver);
+    }
+
+    public static void setWindowSize(int width, int height) {
+        getInstance().getDriver().manage().window().setSize(new Dimension(width, height));
+    }
+
+    public static void maximizeWindow() {
+        getInstance().getDriver().manage().window().maximize();
+    }
+
+    public void quitDriver() {
+        driver.get().quit();
+        driver.remove();
+    }
+}
---
+++ src/main/java/com/logigear/page_objects/LoginPage.java
@@ -0,0 +1,22 @@
+package com.logigear.page_objects;
+
+import com.logigear.elements.Button;
+import com.logigear.elements.TextBox;
+import com.logigear.models.Account;
+import org.openqa.selenium.By;
+
+public class LoginPage extends BasePage {
+
+    //Elements
+    private final TextBox txtEmail = new TextBox(By.cssSelector("input#username"));
+    private final TextBox txtPassword = new TextBox(By.cssSelector("input#password"));
+    private final Button btnLogin = new Button(By.cssSelector("input[title='Login']"));
+
+    //Methods
+    public void login(Account account) {
+        this.btnLogin.scrollToView();
+        this.txtEmail.enterText(account.getEmail());
+        this.txtPassword.enterText(account.getPassword());
+        this.btnLogin.click();
+    }
+}
