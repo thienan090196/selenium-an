145c2fb885b049ef9eb82df2e8a55e4c264ff61b
thienan090196 <thienan090196@gmail.com>
2021-05-06 15:33:11 +0700

    resolve conflict

--- src/main/java/com/logigear/helpers/ErrorMessage.java
+++ src/main/java/com/logigear/helpers/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
 
     public static final String LOGIN_FORM_ERROR = "There was a problem with your login and/or errors exist in your form.";
     public static final String INVALID_EMAIL_OR_PASSWORD = "Invalid username or password. Please try again.";
+    public static final String EMAIL_IS_EXISTED = "This email address is already in use.";
 }
--- src/test/java/com/logigear/tests/RegisterTest.java
+++ src/test/java/com/logigear/tests/RegisterTest.java
@@ -1,6 +1,8 @@
 package com.logigear.tests;
 
+import com.logigear.helpers.Constant;
 import com.logigear.helpers.DataHelper;
+import com.logigear.helpers.ErrorMessage;
 import com.logigear.models.Account;
 import com.logigear.page_objects.HomePage;
 import com.logigear.page_objects.RegisterPage;
@@ -13,6 +15,7 @@ public class RegisterTest extends BaseTest {
     private RegisterPage registerPage = new RegisterPage();
     private Account account;
 
+
     @Test(description = "User can create new account")
     public void TC01() {
         account = new Account(DataHelper.getRandomEmail(), DataHelper.getRandomPassword(), DataHelper.getRandomPid());
@@ -22,5 +25,15 @@ public class RegisterTest extends BaseTest {
         String actualSuccessMessage = registerPage.getSuccessMessage();
         String expectedSuccessMessage = "You're here";
         Assert.assertEquals(actualSuccessMessage, expectedSuccessMessage, "Success message fails to display!");
+    }
+
+    @Test(description = "User cannot create account with an already in-use email")
+    public void TC02() {
+        account = new Account(Constant.EMAIL, Constant.PASSWORD, DataHelper.getRandomPid());
+        homePage.goToRegisterPage();
+        registerPage.register(account);
+
+        String actualErrorMessage = registerPage.getFormErrorMessage();
+        Assert.assertEquals(actualErrorMessage, ErrorMessage.EMAIL_IS_EXISTED, "Error message fails to display!");
     }
 }
